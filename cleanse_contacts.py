b7f2713bb81303728ba9c903204fdd5a26f3a8a0
Jackon Yang <i@jackon.me>
2019-02-27 15:13:19 +0800

    save cleansed contacts into MongoDB

--- cleanse_contacts.py
+++ cleanse_contacts.py
@@ -80,14 +80,26 @@ EXPORT_HEADERS = {
 
 
 def dump_contacts():
+    exists = set()
+    for i in db_msg.contact_cleansed.find():
+        exists.add(i['_id'])
+
     for doc in db_msg.contacts.find():
         for cate in EXPORT_HEADERS.keys():
-            dump_category(
-                doc['myself_name'],
-                cate,
-                doc['data'][cate],
-                doc['fetched_day'],
-            )
+            # dump to CSV files
+            # dump_category(
+            #   doc['myself_name'], cate, doc['data'][cate], doc['fetched_day'])
+
+            # update in MongoDB
+            data = []
+            for i in doc['data'][cate]:
+                _id = i['UserName']
+                if _id not in exists:
+                    exists.add(_id)
+                    i['_id'] = _id
+                    data.append(i)
+            if data:
+                db_msg.contact_cleansed.insert_many(data)
 
 
 def dump_category(owner, name, data, day):
