0fdd8e0c2bd60375558953650e1cf0bffdb7807d
Jackon Yang <i@jackon.me>
2019-02-27 15:21:24 +0800

    load cleansed contacts into MongoDB

--- utils/contacts.py
+++ utils/contacts.py
@@ -8,18 +8,17 @@ contacts_map = dict()
 
 
 def load_contacts():
-    for doc in db_msg.contacts.find():
-        for cate, data in doc['data'].items():
-            for record in data:
-                contacts_map[record['UserName']] = record['NickName']
+    for doc in db_msg.contact_cleansed.find({}, {'NickName': 1}):
+        contacts_map[doc['_id']] = doc['NickName']
 
 
 def get_nickname(username):
-    if len(contacts_map) == 0:
+    if username not in contacts_map:
         load_contacts()
 
     return contacts_map.get(username, username)
 
 
 if __name__ == '__main__':
-    load_contacts()
+    # load_contacts()
+    print(get_nickname('@9b5e4f9b9ba78e43764f4bf4b332bb47'))
